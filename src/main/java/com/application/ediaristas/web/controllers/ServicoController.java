2fd87b29b18805fa0c764024bab560e7ec2d2b51
PauloMtz <pramosmtz@gmail.com>
2021-12-09 17:52:32 -0300

    Cadastrando serviço

--- src/main/java/com/application/ediaristas/web/controllers/ServicoController.java
+++ src/main/java/com/application/ediaristas/web/controllers/ServicoController.java
@@ -2,27 +2,37 @@ package com.application.ediaristas.web.controllers;
 
 import com.application.ediaristas.core.enums.Icone;
 import com.application.ediaristas.core.models.Servico;
+import com.application.ediaristas.core.repositories.ServicoRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/admin/servicos")
 public class ServicoController {
+
+    @Autowired
+    private ServicoRepository repository;
     
     @GetMapping("/cadastrar")
     public ModelAndView cadastrar() {
         var mv = new ModelAndView("admin/servicos/form");
-        mv.addObject("servico_form_controller", new Servico());
-        //mv.addObject("icones_form_controller", Icone.values());
+        mv.addObject("servico", new Servico());
         return mv;
     }
 
-    // aqueles ícones serão disponibilizados para quaisquer views
-    @ModelAttribute("icones_form_controller")
+    @PostMapping("/cadastrar")
+    public String cadastrar(Servico servico) {
+        repository.save(servico);
+        return "redirect:/admin/servicos/cadastrar";
+    }
+
+    @ModelAttribute("icones")
     public Icone[] getIcones() {
         return Icone.values();
     }
