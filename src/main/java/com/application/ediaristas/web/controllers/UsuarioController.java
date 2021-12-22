9e858639a4b971d9604011104a73b05c4a194b5d
PauloMtz <pramosmtz@gmail.com>
2021-12-22 18:54:16 -0300

    Cadastrando usuário

--- src/main/java/com/application/ediaristas/web/controllers/UsuarioController.java
+++ src/main/java/com/application/ediaristas/web/controllers/UsuarioController.java
@@ -1,13 +1,20 @@
 package com.application.ediaristas.web.controllers;
 
+import javax.validation.Valid;
+
+import com.application.ediaristas.web.dtos.FlashMessage;
 import com.application.ediaristas.web.dtos.UsuarioCadastroForm;
 import com.application.ediaristas.web.services.WebUsuarioService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/admin/usuarios")
@@ -29,4 +36,17 @@ public class UsuarioController {
         mv.addObject("usuarioForm", new UsuarioCadastroForm());
         return mv;
     }
+
+    @PostMapping("/cadastrar")
+    public String cadastrar(@Valid @ModelAttribute("usuarioForm") UsuarioCadastroForm usuarioForm,
+        BindingResult result, RedirectAttributes attrs) {
+            if (result.hasErrors()) {
+                return "admin/usuarios/form";
+            }
+
+            service.cadastrar(usuarioForm);
+            attrs.addFlashAttribute("alert", new FlashMessage("alert-success", 
+                "Usuário cadastrado com sucesso!"));
+            return "redirect:/admin/usuarios";
+        }
 }
--- src/main/java/com/application/ediaristas/web/services/WebUsuarioService.java
+++ src/main/java/com/application/ediaristas/web/services/WebUsuarioService.java
@@ -2,8 +2,11 @@ package com.application.ediaristas.web.services;
 
 import java.util.List;
 
+import com.application.ediaristas.core.models.TipoUsuario;
 import com.application.ediaristas.core.models.Usuario;
 import com.application.ediaristas.core.repositories.UsuarioRepository;
+import com.application.ediaristas.web.dtos.UsuarioCadastroForm;
+import com.application.ediaristas.web.mappers.WebUsuarioMapper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,7 +17,16 @@ public class WebUsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private WebUsuarioMapper mapper;
+
     public List<Usuario> listaTodos() {
         return usuarioRepository.findAll();
+    }
+
+    public Usuario cadastrar(UsuarioCadastroForm form) {
+        var model = mapper.toModel(form);
+        model.setTipoUsuario(TipoUsuario.ADMIN);
+        return usuarioRepository.save(model);
     }
 }
